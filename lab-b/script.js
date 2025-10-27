408f1c74feca5671dfb169aa1d0a6614f153d093
Wanish <kw53847@zut.edu.pl>
2025-10-27 23:56:19 +0100

    .

--- lab-b/script.js
+++ lab-b/script.js
@@ -7,26 +7,91 @@ const taskList = document.getElementById("taskList");
 let Todo = {
     tasks: [],
     draw: function() {
+        toSave = [];
+        for (let task of this.tasks) {
+            toSave.push(task.innerHTML);
+        }
+        localStorage.setItem("tasks", JSON.stringify(toSave));
+        
         taskList.innerHTML = "";
         for (let task of this.tasks) {
             taskList.appendChild(task);
+        }
+    },
+    filterTasks: function(){
+        var ul, li, a, i;
+        ul = document.getElementById("taskList");
+        li = ul.getElementsByTagName('li');
+
+        if (searchBar.value.length > 1) {
+            for (i = 0; i < li.length; i++) {
+                a = li[i].querySelector(".taskText").innerHTML;
+                li[i].querySelector(".taskText").innerHTML = li[i].querySelector(".taskText").innerHTML.replaceAll("<mark>", "");
+                li[i].querySelector(".taskText").innerHTML = li[i].querySelector(".taskText").innerHTML.replaceAll("</mark>", "");
+                if (a.includes(searchBar.value)) {
+                    li[i].style.display = "";
+                    li[i].querySelector(".taskText").innerHTML = li[i].querySelector(".taskText").innerHTML.replace(searchBar.value, "<mark>"+searchBar.value+"</mark>");
+                    console.log(li[i].querySelector(".taskText").innerHTML);
+                }
+                else {
+                    li[i].style.display = "none";
+                    //li[i].querySelector(".taskText").innerHTML = li[i].querySelector(".taskText").innerHTML.replace("<mark>", "");
+                    //li[i].querySelector(".taskText").innerHTML = li[i].querySelector(".taskText").innerHTML.replace("</mark>", "");
+                }
+            }
+        }
+        else {
+            for (i = 0; i < li.length; i++) {
+                li[i].style.display = "";
+                li[i].querySelector(".taskText").innerHTML = li[i].querySelector(".taskText").innerHTML.replaceAll("<mark>", "");
+                li[i].querySelector(".taskText").innerHTML = li[i].querySelector(".taskText").innerHTML.replaceAll("</mark>", "");
+            }
         }
     },
     deleteTask: function(listItem) {
         this.tasks.splice(this.tasks.indexOf(listItem), 1);
         this.draw();
     },
-    editTask: function(listItem){
-        const newTask = document.createElement("input");
+    editTaskName: function(listItem){
+        let newTask = document.createElement("input");
+        newTask.addEventListener("blur", function(){   
+            newTaskText = newTask.value.trim();
+
+            let newSpan = document.createElement("span");
+            newSpan.className = "taskText";
+            newSpan.innerHTML = `${newTaskText}`;
+
+            newSpan.addEventListener("click", function(){Todo.editTaskName(listItem)});
+
+            newTask.replaceWith(newSpan);
+        });
+
         let oldTask = listItem.querySelector(".taskText");
+        newTask.value = oldTask.innerHTML;
         oldTask.replaceWith(newTask);
-        newTaskText = newTask.value.trim();
 
-        let newSpan = document.createElement("span");
-        newSpan.className = "taskText";
-        newSpan.innerHTML = `${newTaskText}`;
+        console.log(listItem.innerHTML);
 
-        //newTask.replaceWith(newSpan);
+        this.draw();
+    },
+    editTaskDate: function(listItem){
+        let newDate = document.createElement("input");
+        newDate.type = "date";
+        newDate.addEventListener("blur", function(){
+            newTaskDate = newDate.value;
+
+            let newSpan = document.createElement("span");
+            newSpan.className = "taskDate";
+            newSpan.innerHTML = `${newTaskDate}`;
+
+            newSpan.addEventListener("click", function(){Todo.editTaskDate(listItem)});
+
+            newDate.replaceWith(newSpan);
+        })
+
+        let oldDate = listItem.querySelector(".taskDate");
+        newDate.value = oldDate.innerHTML;
+        oldDate.replaceWith(newDate);
 
         this.draw();
     },
@@ -40,17 +105,47 @@ let Todo = {
         }
 
         var li = document.createElement("li");
-        li.innerHTML = `<input type="checkbox"><span class="taskText">${myTask}</span> <span>${myDate}</span><button type="button" class="deleteButton">Delete</button>`;
+        li.innerHTML = `<input type="checkbox"><span class="taskText">${myTask}</span> <span class="taskDate">${myDate}</span><button type="button" class="deleteButton">Delete</button>`;
 
         let deleteButton = li.querySelector(".deleteButton");
         deleteButton.addEventListener("click", function(){Todo.deleteTask(li)});
 
-        let edit = li.querySelector(".taskText");
-        edit.addEventListener("click", function(){Todo.editTask(li)});
+        let editTask = li.querySelector(".taskText");
+        editTask.addEventListener("click", function(){Todo.editTaskName(li)});
+
+        let editDate = li.querySelector(".taskDate");
+        editDate.addEventListener("click", function(){Todo.editTaskDate(li)});
 
         this.tasks.push(li);
         taskAddText.value = "";
         taskAddDate.value = "";
+
         this.draw()
+    },
+    restorePreviousTasks(){
+        // read previous tasks. If no tasks were found, start with an empty list
+        let storedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+        if (storedTasks.length > 0){
+            for (let oldtask of storedTasks) {
+                var li = document.createElement("li");
+                li.innerHTML = oldtask;
+
+                let deleteButton = li.querySelector(".deleteButton");
+                deleteButton.addEventListener("click", function(){Todo.deleteTask(li)});
+
+                let editTask = li.querySelector(".taskText");
+                editTask.addEventListener("click", function(){Todo.editTaskName(li)});
+
+                let editDate = li.querySelector(".taskDate");
+                editDate.addEventListener("click", function(){Todo.editTaskDate(li)});
+
+                this.tasks.push(li);
+            }
+
+            this.draw();
+        }
     }
 }
+
+Todo.restorePreviousTasks();
